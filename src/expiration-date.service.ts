3c364dce2b8000f0dc4dce03f0d6a598bbe7edfb
EverettZ <everett@zettersten.com>
2017-05-01 11:03:16 -0400

    Adds condition for accidental quick typing.

--- src/expiration-date.service.ts
+++ src/expiration-date.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { IExpirationDateFormatter } from './i-expiration-date';
 
 @Injectable()
-export class ExpirationDateService implements IExpirationDateFormatter{
+export class ExpirationDateService implements IExpirationDateFormatter {
 
-   currYear: string;
+  currYear: string;
 
   constructor() {
     this.currYear = new Date().getFullYear().toString().substr(-2);
   }
 
   formatExpirationDate(val: string) {
+
     val = val.replace(/\D/g, '');
+
     if (val.length > 4) {
       val = val.substr(0, 4);
     }
+    if (val.length == 2 && parseInt(val) > 12) {
+      val = '12';
+    }
+
     let month = val.substr(0, 2) || val;
     let year = val.substr(2, 2) || val.substr(2) || '';
 
